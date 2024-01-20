2061e7f156790b64e1f49b995ea4e15fe0f74e8e
Vlad Liashenko <107635535+Vladyslav-Liashenko@users.noreply.github.com>
2024-01-20 12:31:55 +0200

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,27 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Description } from './Description/Description';
 import { Options } from './Options/Options';
 import { Feedback } from './Feedback/Feedback';
 import { Notification } from './Notification/Notification';
 
 export const App = () => {
-  
-  const [count, setCount] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-    total: 0,
-    positive: 0,
+  const [count, setCount] = useState(() => {
+    const savedCount = window.localStorage.getItem('count');
+    if (savedCount !== null) {
+      return JSON.parse(savedCount);
+    }
+    return {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+      total: 0,
+      positive: 0,
+    };
   });
+
+  useEffect(() => {
+    window.localStorage.setItem('count', JSON.stringify(count));
+  }, [count]);
 
   return (
     <>
@@ -23,14 +32,18 @@ export const App = () => {
         bad={count.bad}
         setCount={setCount}
       />
-      {count.total !==0 ? (<Feedback
+      {count.total !== 0 ? (
+        <Feedback
           good={count.good}
           neutral={count.neutral}
           bad={count.bad}
           total={count.total}
           positive={count.positive}
           setCount={setCount}
-        />) : (<Notification />)}
+        />
+      ) : (
+        <Notification />
+      )}
     </>
   );
 };
